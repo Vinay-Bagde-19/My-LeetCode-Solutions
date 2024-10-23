4e0eb264fe47b3eeffd78236ea811f3742fe9e15
Vinay Bagde <146116197+Vinay-Bagde-19@users.noreply.github.com>
2024-10-23 20:20:54 +0530

    Added Problem No. 66 and 238

---
+++ src/Product_ofArrayExceptSelf.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+/** Problem No. 238 Medium
+ https://leetcode.com/problems/product-of-array-except-self/description/
+
+ Given an integer array nums, return an array answer such that answer[i] is equal to the product of all
+ the elements of nums except nums[i].
+ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ Example 1:
+ Input: nums = [1,2,3,4]
+ Output: [24,12,8,6]
+
+ Example 2:
+ Input: nums = [-1,1,0,-3,3]
+ Output: [0,0,9,0,0]
+ */
+public class Product_ofArrayExceptSelf {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            output[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            output[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return output;
+    }
+}
